d36dd7a26db852425dd0eb6facaeb079dc715c34
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2022-11-05 15:02:48 +0630

    first commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Home</h1>
+    <Nav />
+</template>
+
+<script>
+
+
+import Nav from "../Shared/Nav";
+export default {
+    components:{Nav},
+}
+</script>
+
+<style scoped>
+
+</style>
