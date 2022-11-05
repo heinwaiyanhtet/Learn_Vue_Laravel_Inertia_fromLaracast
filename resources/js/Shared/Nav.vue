afba4c70b31a32ad06e4a611db305c36abe256c1
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2022-11-05 15:43:55 +0630

    Perform non-git requests

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,10 +1,20 @@
 <template>
-    <nav>
-        <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/users">Users</Link></li>
-            <li><Link href="/settings">Settings</Link></li>
-            <li><Link href="/logout">Log out</Link></li>
+    <nav class="mt-6">
+        <ul class="list-disc">
+            <li>
+                <Link href="/" class="text-blue-500 hover:underline">Home</Link>
+            </li>
+            <li>
+                <Link href="/users" class="text-blue-500 hover:underline">Users</Link>
+            </li>
+            <li>
+                <Link href="/settings" class="text-blue-500 hover:underline">Settings</Link>
+            </li>
+            <li>
+                <Link href="/logout" method="post" :data="{foo:'bar'}" class="text-blue-500 hover:underline"
+                      as="button">Log out
+                </Link>
+            </li>
         </ul>
     </nav>
 </template>
@@ -12,8 +22,9 @@
 <script>
 
 import {Link} from "@inertiajs/inertia-vue3";
+
 export default {
-    components:{Link},
+    components: {Link},
 }
 </script>
 <style scoped>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="{{ mix('/js/app.js') }}" defer></script>
     @inertiaHead
 </head>
 <body>
-    @inertia
+    <section class="p-8">
+        @inertia
+    </section>
 </body>
 </html>
