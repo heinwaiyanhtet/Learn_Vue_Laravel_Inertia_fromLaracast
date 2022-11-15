a12416f5b7fae136e983ab0ecabd3518ebb8ec10
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2022-11-15 21:20:26 +0630

    Inertia's Form Helper

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -6,23 +6,23 @@
         <div class="mb-6">
             <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name">Name</label>
             <input v-model="form.name" class="border border-gray-400 p-2 w-full" type="text"  name="name"  id="name" required>
-            <div v-if="errors.name" v-text="errors.name" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.name" v-text="form.errors.name" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label  class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">Email</label>
             <input v-model="form.email" class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
-            <div v-if="errors.email" v-text="errors.email" class="text-red-500 text-xs mt-1"></div>
+            <div v-if="form.errors.email" v-text="form.errors.email" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
             <label  class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">Password</label>
-            <input v-model="form.password" class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
-            <div v-if="errors.password" v-text="errors.password" class="text-red-500 text-xs mt-1"></div>
+            <input v-model="form.password" class="border border-gray-400 p-2 w-full" type="password" name="password" id="password">
+            <div v-if="form.errors.password" v-text="form.errors.password" class="text-red-500 text-xs mt-1"></div>
         </div>
 
         <div class="mb-6">
-            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
+            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500" :disabled="form.processing">
                 Submit
             </button>
         </div>
@@ -31,21 +31,17 @@
 </template>
 
 <script setup>
-import {reactive} from "vue";
-import {Inertia} from "@inertiajs/inertia";
+import { useForm } from "@inertiajs/inertia-vue3";
 
-defineProps({
-    errors:Object
-})
-
-let form = reactive({
-        name:'',
-        email:'',
-        password:''
-    });
+let form = useForm({
+    name:'',
+    email:'',
+    password:''
+});
 
 let submit = () => {
-    Inertia.post('/users',form);
+    form.post('/users')
 };
+
 </script>
 
